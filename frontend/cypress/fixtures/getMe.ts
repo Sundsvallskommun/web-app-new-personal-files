ed84260af87569d906d6595145641e832c65dd01
SK_alicefagerberg <alice.fagerberg@sundsvall.se>
2026-03-19 09:44:10 +0100

    feat: update cypress fixtures

--- frontend/cypress/fixtures/getMe.ts
+++ frontend/cypress/fixtures/getMe.ts
@@ -1,10 +1,17 @@
-import { User } from '@data-contracts/backend/data-contracts';
+import { User, UserRoleEnum } from '@data-contracts/backend/data-contracts';
 import { ApiResponse } from '@services/api-service';
 
 export const getMe: ApiResponse<User> = {
   data: {
-    username: 'username',
-    name: 'Förnamn Efternamn',
+  personId: '',
+  email: 'string',
+  groups: 'string',
+  name: 'Förnamn Efternamn',
+  username: 'för01eft',
+  givenName: 'Förnamn',
+  surname: 'Efternamn',
+  role: UserRoleEnum.PfHrAdmin,
+  permissions:['canRead'],
   },
   message: 'success',
 };
