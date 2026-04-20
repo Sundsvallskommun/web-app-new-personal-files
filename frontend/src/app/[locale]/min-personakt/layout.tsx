2fc2e3ad57e9ad2ab765934f1c2dbd3652b016bf
SK_alicefagerberg <alice.fagerberg@sundsvall.se>
2026-04-20 09:55:38 +0200

    feat: remove unneccesary cookies from layout components and e2e testing

--- frontend/src/app/[locale]/min-personakt/layout.tsx
+++ frontend/src/app/[locale]/min-personakt/layout.tsx
@@ -1,4 +1,3 @@
-import { CookieConsentSection } from '@components/cookie-consent-section/cookie-consent-section.component';
 import DefaultLayout from '@layouts/default-layout/default-layout.component';
 import React from 'react';
 
@@ -6,7 +5,6 @@ export default function MinPersonaktLayout({ children }: { children: React.React
   return (
     <DefaultLayout>
       <div className="flex-grow">{children}</div>
-      <CookieConsentSection />
     </DefaultLayout>
   );
 }
--- frontend/src/layouts/default-layout/default-layout.component.tsx
+++ frontend/src/layouts/default-layout/default-layout.component.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { OverviewSidebar } from '@components/sidebar/overview-sidebar.component';
-import { CookieConsent, Link } from '@sk-web-gui/react';
+import { Link } from '@sk-web-gui/react';
 import NextLink from 'next/link';
 import { useTranslation } from 'react-i18next';
 
@@ -41,42 +41,6 @@ export default function DefaultLayout({ children }: DefaultLayoutProps) {
           <div className="max-w-[1024px] w-full">{children}</div>
         </div>
       </div>
-
-      <CookieConsent
-        title={t('layout:cookies.title', { app: process.env.NEXT_PUBLIC_APP_NAME })}
-        body={
-          <p>
-            {t('layout:cookies.description')}{' '}
-            <NextLink href="/kakor" passHref legacyBehavior>
-              <Link>{t('layout:cookies.read_more')}</Link>
-            </NextLink>
-          </p>
-        }
-        cookies={[
-          {
-            optional: false,
-            displayName: t('layout:cookies.necessary.displayName'),
-            description: t('layout:cookies.necessary.description'),
-            cookieName: 'necessary',
-          },
-          {
-            optional: true,
-            displayName: t('layout:cookies.func.displayName'),
-            description: t('layout:cookies.func.description'),
-            cookieName: 'func',
-          },
-          {
-            optional: true,
-            displayName: t('layout:cookies.stats.displayName'),
-            description: t('layout:cookies.stats.description'),
-            cookieName: 'stats',
-          },
-        ]}
-        resetConsentOnInit={false}
-        onConsent={() => {
-          // FIXME: do stuff with cookies?
-        }}
-      />
     </div>
   );
 }
