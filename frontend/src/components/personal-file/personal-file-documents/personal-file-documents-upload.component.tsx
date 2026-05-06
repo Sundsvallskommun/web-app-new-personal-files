cae2ab018bc64da12868acb81bf5f3505183e4d2
SK_alicefagerberg <alice.fagerberg@sundsvall.se>
2026-05-06 15:01:45 +0200

    feat: upload document component

---
+++ frontend/src/components/personal-file/personal-file-documents/personal-file-documents-upload.component.tsx
@@ -0,0 +1,216 @@
+import {
+  Button,
+  Modal,
+  FormLabel,
+  FormControl,
+  FileUpload,
+  Select,
+  Input,
+  useSnackbar,
+  FormErrorMessage,
+} from '@sk-web-gui/react';
+import { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { useDocumentStore } from '@services/document-service/document-service';
+import { useUserStore } from '@services/user-service/user-service';
+import { CreateDocument } from '@interfaces/document/document';
+import { useEmployeeStore } from '@services/employee-service/employee-service';
+import { Employment } from '@interfaces/employee/employee';
+
+export interface PersonalFileUploadDocumentFormModel {
+  attachment: Array<File>;
+  attachmentCatgory: string;
+}
+
+const formSchema = yup.object({
+  attachment: yup
+    .mixed<File[]>()
+    .test('required', 'Välj en fil', (value) => !!value)
+    .required('Välj en fil'),
+  attachmentCatgory: yup.string().required('Välj en kategori'),
+});
+
+export const PersonalFileDocumentsUpload: React.FC<{ emp: Employment; personId: string }> = ({ emp, personId }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const user = useUserStore((s) => s.user);
+  const uploadDocument = useDocumentStore((s) => s.uploadDocument);
+  const getDocuments = useDocumentStore((s) => s.getDocumentList);
+  const documentTypes = useDocumentStore((s) => s.documentTypes);
+  const selectedEmployment = useEmployeeStore((s) => s.selectedEmployment);
+
+  const [fileError, setFileError] = useState<string>('');
+
+  const toastMessage = useSnackbar();
+
+  const closeHandler = () => {
+    reset();
+    setIsOpen(false);
+  };
+
+  const {
+    register,
+    control,
+    watch,
+    reset,
+    setValue,
+    getValues,
+    formState,
+    trigger,
+    formState: { errors, isDirty },
+  } = useForm<PersonalFileUploadDocumentFormModel>({
+    resolver: yupResolver(formSchema),
+    defaultValues: {
+      attachment: undefined,
+      attachmentCatgory: 'EMPLOYMENT_CERTIFICATE',
+    },
+    mode: 'onChange', // NOTE: Needed if we want to disable submit until valid
+  });
+
+  const attachment = watch('attachment');
+
+  useEffect(() => {
+    const allowedTypes = ['pdf'];
+    if (getValues()?.attachment) {
+      const attachmentTypeget: string = getValues()?.attachment[0]?.name.split('.').pop() || '';
+      if (!allowedTypes.includes(attachmentTypeget) && attachmentTypeget !== '') {
+        setFileError('Fel filtyp, välj en pdf');
+      } else {
+        setFileError('');
+      }
+    }
+  }, [getValues]);
+
+  return (
+    <div>
+      <Button data-cy="upload-document" variant="tertiary" onClick={() => setIsOpen(true)}>
+        Ladda upp
+      </Button>
+
+      <Modal className="max-w-[320px] w-full" show={isOpen} onClose={closeHandler}>
+        <h2 className="text-base">Ladda upp dokument</h2>
+        <Modal.Content className="flex flex-col gap-20">
+          <FormControl className="w-full">
+            <FormLabel className="">
+              <div role="input" className="flex justify-between w-full">
+                <span className="text-label-small">Vald fil</span>{' '}
+                <span className="sk-link text-vattjom-text-primary font-normal hover:cursor-pointer">Bläddra</span>
+              </div>
+              <Input
+                className="hidden"
+                type="file"
+                placeholder="Välja fil att lägga till"
+                {...register('attachment')}
+                //allowReplace={false}
+              />
+              <Input
+                className="w-full"
+                value={getValues()?.attachment ? getValues()?.attachment[0]?.name : ''}
+                readOnly
+                placeholder="Bläddra bland dokument"
+              />
+            </FormLabel>
+          </FormControl>
+          <FormControl className="w-full">
+            <FormLabel className="text-label-small">Tilldela kategori</FormLabel>
+            <Select
+              onChange={(e) => {
+                setValue('attachmentCatgory', e.target.value, { shouldDirty: true });
+                trigger('attachmentCatgory');
+              }}
+              value={getValues().attachmentCatgory ? getValues().attachmentCatgory : ''}
+              className="w-full"
+            >
+              {documentTypes?.map((type, idx) => {
+                return (
+                  <Select.Option key={`type-${idx}`} value={type.type}>
+                    {type.displayName}
+                  </Select.Option>
+                );
+              })}
+            </Select>
+          </FormControl>
+          {fileError !== '' && <FormErrorMessage className="text-error">{fileError}</FormErrorMessage>}
+        </Modal.Content>
+        <Modal.Footer>
+          <Button
+            className="w-full"
+            onClick={() => {
+              const body: CreateDocument = {
+                createdBy: user.username,
+                confidentiality: {
+                  confidential: false,
+                },
+                archive: false,
+                description: `${documentTypes ? documentTypes.find((t) => t.type === getValues().attachmentCatgory)?.displayName : 'Anställningsbevis'} för timavlönad`,
+                metadataList: [
+                  {
+                    key: 'employmentId',
+                    value: `${emp.employmentId}`,
+                  },
+                  {
+                    key: 'partyId',
+                    value: `${personId}`,
+                  },
+                  {
+                    key: 'startDate',
+                    value: `${emp.startDate}`,
+                  },
+                  {
+                    key: 'endDate',
+                    value: `${emp.endDate}`,
+                  },
+                ],
+                type: getValues().attachmentCatgory,
+              };
+
+              return uploadDocument(body, getValues().attachment[0])
+                .then(async (res) => {
+                  if (res.data) {
+                    toastMessage({
+                      position: 'bottom',
+                      closeable: false,
+                      message: 'Dokumentet laddades upp',
+                      status: 'success',
+                    });
+
+                    await getDocuments([
+                      {
+                        key: 'employmentId',
+                        matchesAny: [selectedEmployment.empRowId ?? ''],
+                      },
+                      {
+                        key: 'partyId',
+                        matchesAny: [personId || ''],
+                      },
+                    ]);
+                    closeHandler();
+                    reset({ attachment: undefined, attachmentCatgory: 'EMPLOYMENT_CERTIFICATE' });
+                  }
+                })
+                .catch((e) => {
+                  toastMessage({
+                    position: 'bottom',
+                    closeable: false,
+                    message: 'Dokumentet gick inte att ladda upp',
+                    status: 'error',
+                  });
+                });
+            }}
+            disabled={
+              fileError.length !== 0 ||
+              (!formState.dirtyFields.attachment && !formState.dirtyFields.attachmentCatgory) ||
+              getValues().attachment === undefined ||
+              getValues().attachmentCatgory === undefined ||
+              !getValues().attachmentCatgory.length
+            }
+          >
+            {' '}
+            Ladda upp{' '}
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+};
