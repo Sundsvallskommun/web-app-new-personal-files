091501554c9196d7c0d74ff5a110f19c69c714b9
aliceringstromsk <alice.ringstrom@sundsvall.se>
2026-06-11 11:17:09 +0200

    fix: move max file size to env

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -11,7 +11,8 @@ export const PATH = {
   myPersonalFile: 'min-personakt',
 };
 
-export const MAX_FILE_SIZE = 1 * 1024 * 1024;
+export const MAX_FILE_SIZE_MB = Number(process.env.NEXT_PUBLIC_MAX_FILE_SIZE);
+export const MAX_FILE_SIZE_BYTES = MAX_FILE_SIZE_MB * 1024 * 1024;
 
 export const UPLOAD_DOCUMENT_DEFAULT_VALUES: PersonalFileUploadDocumentFormModel = {
   attachment: [],
